3da6a2a1a272ad80c6774b34d5eaa1dc455fdf3c
BMO <rtbmo23@gmail.com>
2026-03-01 19:22:25 +0530

    Create distortionRules.js

---
+++ src/utils/distortionRules.js
@@ -0,0 +1,37 @@
+export const rules = [
+  {
+    type: "Overgeneralization",
+    keywords: [
+      "always",
+      "never",
+      "everyone",
+      "nobody",
+      "everything",
+      "nothing",
+      "all the time",
+      "completely",
+      "totally"
+    ],
+    explanation:
+      "This thought uses absolute language and assumes something is true in all situations.",
+    suggestion:
+      "Is this really true in every single case? What evidence suggests otherwise?"
+  },
+  {
+    type: "Catastrophizing",
+    keywords: [
+      "what if",
+      "ruin",
+      "disaster",
+      "terrible",
+      "worst",
+      "awful",
+      "everything will go wrong",
+      "it will fail"
+    ],
+    explanation:
+      "This thought predicts the worst possible outcome without considering more realistic possibilities.",
+    suggestion:
+      "What is the most realistic outcome instead of the worst one?"
+  }
+];
